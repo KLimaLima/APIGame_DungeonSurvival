4abcf21709b04f662e587fc0b39802c2bed73cd8
KOKYUYUAN <163385875+KOKYUYUAN@users.noreply.github.com>
2024-06-04 18:06:47 +0800

    update the rest part(kok)

--- index.js
+++ index.js
@@ -2,10 +2,112 @@ const bcrypt = require('bcrypt')
 const express = require('express');
 const leaderboardRouter=express.Router();
 module.exports=leaderboardRouter;
-
+const client = new MongoClient(uri);
+var jwt = require('jsonwebtoken');
+const { ObjectId } = require('mongodb');
 
 //app.use(express.json());
 
+//part for token verification
+
+client.connect()
+    .then(() => {
+        app.listen(3000, () => {
+            console.log(`Server is running on port 3000`);
+        });
+    })
+    .catch(err => console.error(err));
+
+    function verifyToken(req, res, next) {
+      const bearerHeader = req.headers['authorization'];
+      if (typeof bearerHeader !== 'undefined') {
+          const bearer = bearerHeader.split(' ');
+          const bearerToken = bearer[1];
+          req.token = bearerToken;
+          jwt.verify(req.token, 'chiikawaaaaaaa', (err, authData) => {
+              if(err) {
+                  res.sendStatus(403);
+              } else {
+                  req.authData = authData;
+                  next();
+              }
+          });
+      } else {
+          res.sendStatus(403);
+      }
+  }
+
+
+
+leaderboardRouter.post('/account/login',async(req,res) => { 
+    // step #1:req.body.username
+      let result = await client.db("ds_db").collection("account").findOne({
+        player: req.body.player
+    })
+    console.log(result);
+    console.log(req.body);
+    if(!req.body.player || !req.body.password){
+      res.status(404).send('Please provide username and password')
+    }
+    else if(req.body.player != null && req.body.password != null){
+    
+      if(result){
+        //step2:if user exists, check if password is correct
+        if(bcrypt.compareSync(req.body.password,result.password)==true){
+          var token = jwt.sign(
+            { _id: result._id, player: result.player}, 
+            'chiikawaaaaaaa',
+          
+          { expiresIn: 60*60  });
+          //paaword is correct
+          res.send(token);
+        } else{
+          //password is incorrect
+          res.status(404).send('Wrong Password');
+        
+        }
+      }else{
+        //step3:if user not found
+        res.send('User not found');
+      
+      }
+  }
+  })
+  
+  
+  
+  
+  leaderboardRouter.get('/account/:id',verifyToken, async(req, res) => {
+    if (req.authData._id != req.params.id) {
+      res.send('User is not authorized')
+    
+    }
+    let auth = req.headers.authorization
+    console.log(auth)
+  
+    let authSplitted = auth.split(' ')
+    console.log(authSplitted)
+  
+    let token = authSplitted[1]
+    console.log(token)
+  
+    let decoded = jwt.verify(token, 'chiikawaaaaaaa')
+    console.log(decoded)
+  
+    if (req.authData._id != req.params.id) {
+      res.send('User is not authorized')
+    }
+    else{
+    let result= await client.db("ds_db").collection("account").findOne({
+      _id: new ObjectId(req.params.id)
+    })
+    res.send(`Player name : ${result.player}\n _id: ${result._id}`)
+    
+  }
+  })
+
+
+// Get the leaderboard
 
 leaderboardRouter.get('/leaderboard', async(req, res) => {
     let LatestLB = await client.db("da_db").collection("account")
@@ -15,7 +117,15 @@ leaderboardRouter.get('/leaderboard', async(req, res) => {
 
     res.json(LatestLB);
 });
-leaderboardRouter.post('/register',async(req,res)=>{
+
+
+
+
+//account registration
+
+
+
+leaderboardRouter.post('/account/register',async(req,res)=>{
     let Exists= await client.db("ds_db").collection("account").findOne({
         player:req.body.player
     });
@@ -27,41 +137,49 @@ leaderboardRouter.post('/register',async(req,res)=>{
         let result= await client.db("ds_db").collection("account").insertOne({
             player:req.body.player,
             password:hash
+       
         });
-
         let result1 = await client.db('ds_db').collection('almanac').aggregate([{$sample:{size:1}}]).toArray();
-        let document = result1[0]; // get the first document from the result array
-        let skills = document.skill;
+      let document = result1[0]; // get the first document from the result array
+      let skills = document.skill;
 
-        // Generate a random index
-        let randomIndex = Math.floor(Math.random() * skills.length);
+      // Generate a random index
+      let randomIndex = Math.floor(Math.random() * skills.length);
 
-        // Get a random skill
-        let randomSkill = skills[randomIndex];
+      // Get a random skill
+      let randomSkill = skills[randomIndex];
 
 
-        
-
-        let statPlayer= await client.db("ds_db").collection("stat").insertOne({
-            playerID:req.body.player,
-            inventory:0,
-            attack_action:10,
-            current_enemy:document.enemy,
-            current_score:0,
-            enemy_health:document.base_health,
-            enemy_next_move:randomSkill.attack_name,
-            evade_acrion:5,
-            heath_pts:10
       
-       })
+
+      let statPlayer= await client.db("ds_db").collection("stat").insertOne({
+          playerID:req.body.player,
+          inventory:0,
+          attacl_action:10,
+          current_enemy:document.enemy,
+          current_score:0,
+          enemy_health:document.base_health,
+          enemy_next_move:randomSkill.attack_name,
+          evade_acrion:5,
+          heath_pts:10
+    
+     })
+    
+    }
+    
     res.send({message:"Account created successfully, please remember your player id"});
-})
-leaderboardRouter.post('/forgetuserID', async(req, res) => {
+    
+ })
+
+
+//forget userid
+
+leaderboardRouter.post('/account/forgetuserID', async(req, res) => {
     let result = await client.db("ds_db").collection("account").findOne({
-        player: req.body.player,
-        password: req.body.password
+      player: req.body.player
+        
     })
-    if(!req.body.username || !req.body.password){
+    if(!req.body.player || !req.body.password){
         res.status(404).send('Please provide username and password')
       }
       else if(req.body.player != null && req.body.password != null){
@@ -82,59 +200,54 @@ leaderboardRouter.post('/forgetuserID', async(req, res) => {
         
         }
     }
+    
+});
 
-    leaderRouter.patch ("/changepassword", async (req, res) => {
-    
 
-        let findUser = await client.db('ds_db').collection('account').findOne({player:req.body.player});
-    
-        if (findUser) {
-    
-            if (bcrypt.compareSync(req.body.password, findUser.password) == true){ //compare the password with the hashed password in the database
-            
-            req.body.password = bcrypt.hashSync(req.body.newpassword, 10); //hash the new password
-            await client.db('ds_db').collection('account').updateOne({player:req.body.player}, {$set: {password:req.body.password}}); //update the password in the database
-            res.send('password changed successfully');
-            }
-    
-            else { //password is incorrect
-                res.status(401).send('password incorrect')
-              }
-    
-        }
-    
-        else { //not found
-    
-            res.send('user not found')
-      
-          }
+//update or change password for current account
+leaderboardRouter.patch ("/account/changepassword" ,async (req, res) => {
+    if (!req.body.newpassword) {
+      return res.status(400).send('New password is required');
+  }
+
+    let findUser = await client.db('ds_db').collection('account').findOne({player:req.body.player});
+
+    if (findUser) {
+
+        if (bcrypt.compareSync(req.body.password, findUser.password) == true){ //compare the password with the hashed password in the database
         
-        });
+        req.body.password = bcrypt.hashSync(req.body.newpassword, 10); //hash the new password
+        await client.db('ds_db').collection('account').updateOne({player:req.body.player}, {$set: {password:req.body.password}}); //update the password in the database
+        res.send('password changed successfully');
+        }
 
-    leaderboardRouter.delete('/delete', async(req, res) => {
-     // First, find the user
-        let user = await client.db("ds_db").collection("account").findOne({
-             player: req.body.player
-        });
+        else { //password is incorrect
+            res.status(401).send('password incorrect')
+          }
 
-    // If user doesn't exist, return an error
-        if (!user) {
-          return res.status(404).send('User not found');
     }
 
-     // Check if the password is correct
-    const isPasswordCorrect = bcrypt.compareSync(req.body.password, user.password);
-        if (!isPasswordCorrect) {
-         return res.status(403).send('Please insert correct playername and password');
-    }
+    else { //not found
 
-    // If password is correct, delete the user
-      else{
-        let del= await client.db("ds_db").collection("account").deleteOne({
-         player: req.body.player    });
-    }
-    res.send("Account Deleted Successfully");
-
-});
+        res.send('user not found')
+  
+      }
     
-});
+    });
+
+
+//delete current account
+leaderboardRouter.delete('/account/delete/:id',verifyToken, async(req, res) => {
+        if(req.authData._id != req.params.id){
+          res.send('User is not authorized')
+        }
+        else{
+        let result= await client.db("ds_db").collection("account").deleteOne({
+            _id: new ObjectId(req.params.id)
+        })
+        
+        }
+        
+        res.send("Account Deleted Successfully");
+    
+    });
