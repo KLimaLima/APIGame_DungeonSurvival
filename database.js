87f51901d4b7c50495247fbeb42d7abc1f6b13e8
KLimaLima <138441635+KLimaLima@users.noreply.github.com>
2024-06-24 15:30:18 +0800

    Revert "wrong password (#49)"
    This reverts commit e2914ef09b817f0ae877ee29edcbacb9d7f2343c.

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://dsdbkhairulpjn:${process.env.DBURI}@clusterdsofficial.vtlwh6u.mongodb.net/?retryWrites=true&w=majority&appName=ClusterDSOfficial`;
+const uri = `${process.env.DBURI}`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
